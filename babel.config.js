87ccc914dc9cc40dd14b3f45aa928c7aa08ea2ea
Nick Setzer <nicksetzer@gmail.com>
2019-04-14 06:29:16 -0400

    update package.json

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = function(api) {
-  api.cache(true);
+  api.cache(false);
 
   const plugins = [
-    ['@babel/plugin-transform-flow-strip-types'],
-    ['@babel/plugin-proposal-class-properties'],
-    ['@babel/transform-runtime']
+    '@babel/plugin-transform-flow-strip-types',
+    ['@babel/plugin-proposal-class-properties', { "loose": true }],
+    '@babel/transform-runtime'
   ];
 
   const presets = [
-    'babel-preset-expo',
+    'babel-preset-expo',  "@babel/preset-env", "@babel/preset-react"
   ];
 
   return {
